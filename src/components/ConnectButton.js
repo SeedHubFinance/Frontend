0a90852cef51ac661c35601857026e31b06b5a91
khanashes <hamzaashes@gmail.com>
2021-11-19 14:52:09 +0500

    state managment

--- src/components/ConnectButton.js
+++ src/components/ConnectButton.js
@@ -1,47 +1,32 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
+import { initialiseMetamask } from "../utils/connectionHelpers";
 
 const ConnectedButton = () => {
-  const [account, setAccount] = useState("");
-  let [web3, setWeb3] = useState(null);
-
-  useEffect(() => {
-    checkAccount();
-  }, []);
-
-  // invoke to connect to wallet account
-  async function activate() {
-    if (window.ethereum) {
-      try {
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-        checkAccount();
-      } catch (err) {
-        console.log("user did not add account...", err);
-      }
+  const [address, setAddress] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  // const [provider, setProvider] = useState(null);
+  const handleConnectWallet = async (e) => {
+    e.preventDefault();
+    console.log("Hello 2");
+    const { isConnected, address, provider } = await initialiseMetamask();
+    setAddress(address);
+    setIsConnected(isConnected);
+    if (!isConnected || !address || !provider) {
+      return console.log("Connection error");
     }
-  }
+    window.web3 = new Web3(provider);
+  };
 
-  // invoke to check if account is already connected
-  async function checkAccount() {
-    let web3 = new Web3(window.ethereum);
-    setWeb3(web3);
-    const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
-  }
-
-  function handleConnectWallet() {
-    activate();
-  }
-
-  return account ? (
+  return isConnected ? (
     <div className="container">
       <div className="row">
         <div className="col">
           <p>
-            {account &&
-              `${account.slice(0, 6)}...${account.slice(
-                account.length - 4,
-                account.length
+            {address &&
+              `${address.slice(0, 6)}...${address.slice(
+                address.length - 4,
+                address.length
               )}`}
           </p>
         </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,5 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
-
 const Footer = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="inherit" padding="">
-        <Typography
-          variant="h3"
-          noWrap
-          component="div"
-          sx={{ display: { xs: "none", sm: "block" } }}
-        >
-          MUI
-        </Typography>
-      </AppBar>
-    </Box>
-  );
+  return <h1>Footer</h1>;
 };
 
 export default Footer;
