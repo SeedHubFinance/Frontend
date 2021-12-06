07cedad2e0dd4f28b85557269a6ede38332d97e1
Safi ullah <safi.ullah@invozone.com>
2021-12-06 14:20:16 +0500

    ABIs added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/home";
+import TestPage from "./pages/testPool"
+import FixedPriceSalePage from "./pages/testFixedPriceSwap"
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <FixedPriceSalePage />
     </div>
   );
 }
