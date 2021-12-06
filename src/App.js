f01d6025c5794de5f7e35fb47e544f9aa16c5a3b
Safi ullah <safi.ullah@invozone.com>
2021-12-06 15:07:10 +0500

    merged

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Home from "./pages/Home/home";
 import { Web3Provider } from "./context/web3Context";
 import "./App.scss";
@@ -10,17 +9,6 @@ function App() {
         <Home />
       </div>
     </Web3Provider>
-=======
-import Home from "./pages/home";
-import TestPage from "./pages/testPool"
-import FixedPriceSalePage from "./pages/testFixedPriceSwap"
-
-function App() {
-  return (
-    <div className="App">
-      <FixedPriceSalePage />
-    </div>
->>>>>>> contracts-demo
   );
 }
 
