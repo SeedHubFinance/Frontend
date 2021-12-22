eaa8b77a382a4c82529a5061b57f8f0b73ff9487
Safi ullah <safi.ullah@invozone.com>
2021-12-22 12:14:31 +0500

    cards update

--- src/components/Cardlist/Cardlist.js
+++ src/components/Cardlist/Cardlist.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import TokenSaleCard from "../TokenSaleCard/TokenSaleCard";
 import "./Cardlist.scss";
+import { Web3Context } from "../../context/web3Context";
+
+import {
+  fixedSwapABI,
+  fixedSwapContractAddress,
+} from "../../contracts/FixedSwap";
 
 const Cardlist = (props) => {
+  const [web3, setWeb3] = useContext(Web3Context);
+  const [pools, setPools] = useState([]);
+  const getAllPools = async () => {
+    if (web3) {
+      let fixedSwapContract = new web3.eth.Contract(
+        fixedSwapABI,
+        fixedSwapContractAddress
+      );
+
+      let addresses = await web3?.eth.getAccounts();
+
+      await fixedSwapContract.methods
+        .getAllPools()
+        .call({ from: addresses[0] })
+        .then((data) => setPools(data));
+    }
+  };
+
+  useEffect(() => {
+    getAllPools();
+  }, [web3]);
+
   return (
     <div className="cardlist">
-      <TokenSaleCard />
-      <TokenSaleCard />
+      {pools.map((pool, index) => {
+        return (
+          <TokenSaleCard
+            index={index}
+            sellToken={pool.sellToken}
+            swapRatio={pool.swapRatio}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/components/TokenSaleCard/TokenSaleCard.js
+++ src/components/TokenSaleCard/TokenSaleCard.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Card, ProgressBar, Button } from "react-bootstrap";
 import "./TokenSaleCard.scss";
 
-const Tokensalecard = () => {
+const Tokensalecard = ({ index, sellToken, swapRatio }) => {
   return (
     <Fragment>
       <Card className="mb-3">
@@ -13,7 +13,7 @@ const Tokensalecard = () => {
               <span>
                 <div className="dot me-2"></div> Live
               </span>
-              <p className="d-md-none"># 6189</p>
+              <p className="d-md-none"># {index}</p>
             </div>
             <div className="card-title text-break">ETH/SHIB</div>
           </div>
@@ -21,7 +21,11 @@ const Tokensalecard = () => {
             <div className="card-content pt-md-0 pt-4">
               <div>
                 <span>Address</span>
-                <p>0xA447...C354</p>
+                <p>
+                  {(sellToken?.substr(0, 4) || "") +
+                    "..." +
+                    (sellToken?.substr(-4, 4) || "")}
+                </p>
               </div>
               <div>
                 <span>Pair</span>
@@ -29,7 +33,7 @@ const Tokensalecard = () => {
               </div>
               <div>
                 <span>Swap Ratio</span>
-                <p>200 : 1</p>
+                <p>{swapRatio} : 1</p>
               </div>
               <div>
                 <span>Price</span>
