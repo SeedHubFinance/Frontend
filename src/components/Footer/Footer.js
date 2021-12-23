bc2563b7e443345eaf82037a0f11431161e85749
Taimoor Aslam <taimooraslam91@gmail.com>
2021-12-23 18:24:06 +0500

    UI main changes complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,14 +15,24 @@ const Footer = () => {
         <Row className="w-100 g-0 align-items-center text-center">
           <Col lg={4}>
             <div className="footer-icons justify-content-lg-start justify-content-center">
-              <MediumIcon />
-              <TwitterIcon />
-              <TelegramIcon />
-              <MsgIcon />
+              <a href="https://medium.com/@seedhub/" target="_blank">
+                <MediumIcon />
+              </a>
+              <a href="https://twitter.com/seedhubnetwork" target="_blank">
+                <TwitterIcon />
+              </a>
+              <a href="https://t.me/seedhub" target="_blank">
+                <TelegramIcon />
+              </a>
+              <a href="https://github.com/SeedHubnetwork" target="_blank">
+                <MsgIcon />
+              </a>
             </div>
           </Col>
           <Col lg={2} className="my-lg-0 my-3">
-            <p>Fixed-Swap Docs</p>
+            <a href="https://docs.seedhub.network/" target="_blank">
+              Fixed-Swap Docs
+            </a>
           </Col>
           <Col lg={6}>
             <p className="text-lg-end">
