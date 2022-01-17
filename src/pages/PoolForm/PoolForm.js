9eb0f4b70b2592dc5dbded657738825f51271ba9
khanashes <hamzaashes@gmail.com>
2022-01-18 00:23:04 +0500

    mutiple clock

--- src/pages/PoolForm/PoolForm.js
+++ src/pages/PoolForm/PoolForm.js
@@ -21,23 +21,32 @@ import { ReactComponent as MaxIcon } from "../../Assets/Images/max.svg";
 import "./PoolForm.scss";
 
 // Countdown Timer
-const Completionist = () => <span>0 d : 0 h : 0 m : 0 s</span>;
-const renderer = ({ days, hours, minutes, seconds, completed }) => {
-  if (completed) {
-    // Render a complete state
-    return <Completionist />;
-  } else {
-    // Render a countdown
-    return (
-      <span>
-        <span className="mx-2">{days} d</span>:
-        <span className="mx-2">{hours} h</span>:
-        <span className="mx-2">{minutes} m</span>:
-        <span className="mx-2">{seconds} s</span>
-      </span>
-    );
-  }
+// const Completionist = () => <span>0 d : 0 h : 0 m : 0 s</span>;
+const renderer = ({ days, hours, minutes, seconds }) => {
+  console.log(seconds);
+  // Render a countdown
+  return (
+    <span>
+      <span className="mx-2">{days} d</span>:
+      <span className="mx-2">{hours} h</span>:
+      <span className="mx-2">{minutes} m</span>:
+      <span className="mx-2">{seconds} s</span>
+    </span>
+  );
 };
+
+// const renderer2 = ({ days, hours, minutes, seconds }) => {
+//   console.lo
+//   // Render a countdown
+//   return (
+//     <span>
+//       <span className="mx-2">{days} d</span>:
+//       <span className="mx-2">{hours} h</span>:
+//       <span className="mx-2">{minutes} m</span>:
+//       <span className="mx-2">{seconds} s</span>
+//     </span>
+//   );
+// };
 
 const Fixedswap = (props) => {
   const [web3, setWeb3] = useContext(Web3Context);
@@ -45,11 +54,12 @@ const Fixedswap = (props) => {
   const [error, setError] = useState(null);
   const [tokenSymbol, setTokenSymbol] = useState(null);
   const [tokenDecimals, setTokenDecimals] = useState(0);
+  const [isClosed, setIsClosed] = useState(false);
+  const [isExpired, setIsExpired] = useState(false);
 
   const location = useLocation();
   const [amount, setAmount] = useState();
   const [bidPrice, setPriceAmount] = useState(0);
-
   const [isWeb3Connected, setWeb3Status] = useState(false);
   // const statusRef = useRef("");
   // useEffect(() => {
@@ -58,12 +68,11 @@ const Fixedswap = (props) => {
   //     ? (statusRef.current.innerText = "Closed")
   //     : (statusRef.current.innerText = "Live");
   // }, []);
-  const date = new Date(location.state.endAuctionAt * 1000);
-  const statusObj =
-    date < new Date()
-      ? { status: "Closed", isClosed: true }
-      : { status: "Live", isClosed: false };
-
+  // const date = new Date(location.state.endAuctionAt * 1000);
+  // const statusObj =
+  //   date < new Date()
+  //     ? { status: "Closed", isClosed: true }
+  //     : { status: "Live", isClosed: false };
   const getSymbol = async () => {
     if (web3?.eth) {
       const tokenContract = new web3.eth.Contract(
@@ -103,10 +112,22 @@ const Fixedswap = (props) => {
     }
   };
 
+  // useEffect(() => {
+  //   setIsClosed(false);
+  // }, [isClosed]);
+
   useEffect(() => {
     getUserWalletAddress();
     getSymbol();
     getDecimals();
+    const date = new Date(location.state.endAuctionAt * 1000);
+    if (date < new Date()) {
+      setIsClosed(true);
+    }
+    const claimDate = new Date(location.state.claimAuctionFundsAt * 1000);
+    if (claimDate < new Date()) {
+      setIsExpired(true);
+    }
   }, [web3, address]);
 
   function toFixed(x) {
@@ -198,12 +219,10 @@ const Fixedswap = (props) => {
                   <div className="card-head mb-4 d-flex flex-column">
                     <span>
                       <div
-                        className={`${
-                          statusObj.isClosed ? "dotClose" : "dotLive"
-                        } me-2`}
+                        className={`${isClosed ? "dotClose" : "dotLive"} me-2`}
                       />
-                      <div className={statusObj.isClosed && "closed"}>
-                        {statusObj.status}
+                      <div className={isClosed && "closed"}>
+                        {isClosed ? "Closed" : "Live"}
                       </div>
                     </span>
                     <p>
@@ -242,64 +261,104 @@ const Fixedswap = (props) => {
                   </div>
                 </div>
               </Col>
-              <Col
-                md={6}
-                lg={5}
-                className="offset-lg-2 mt-4 mt-md-0 p-4 p-md-5 bg-off"
-              >
-                <div className="form-heading text-center mb-4">
-                  Join The Pool
-                </div>
-                <div className="text-center fs-6">
-                  <Countdown
-                    date={new Date(location.state.endAuctionAt * 1000)}
-                    renderer={renderer}
-                  />
-                </div>
-                <div className="divder"></div>
-                <div className="d-flex justify-content-between">
-                  <span className="label">Amount</span>
-                  <span className="label">Balance: 0 ETH</span>
-                </div>
-                <div className="d-flex">
-                  <input
-                    className="custom-input me-3"
-                    type="number"
-                    required
-                    type="number"
-                    name="amount"
-                    placeholder="Bid Price"
-                    onChange={(e) => calculateAmountFromPrice(e.target.value)}
-                  />
-                  <input
-                    className="custom-input ms-3"
-                    disabled
-                    type="number"
-                    required
-                    type="number"
-                    name="amount"
-                    placeholder="Bid Amount"
-                    value={amount}
-                  />
-                </div>
-                <Button
-                  onClick={handleClick}
-                  disabled={amount > 0 && isWeb3Connected ? false : true}
-                  className="sub-btn mt-3"
+              {!isClosed ? (
+                <Col
+                  md={6}
+                  lg={5}
+                  className="offset-lg-2 mt-4 mt-md-0 p-4 p-md-5 bg-off"
                 >
-                  GO
-                </Button>
-                <p
-                  style={{
-                    color: "red",
-                    marginTop: "5px",
-                    textAlign: "center",
-                    fontSize: "14px",
-                  }}
+                  <div className="form-heading text-center mb-4">
+                    Join The Pool
+                  </div>
+                  <div className="text-center fs-6">
+                    <Countdown
+                      key={0}
+                      date={new Date(location.state.endAuctionAt * 1000)}
+                      renderer={renderer}
+                      onComplete={() => {
+                        setIsClosed(true);
+                      }}
+                    />
+                  </div>
+                  <div className="divder"></div>
+                  <div className="d-flex justify-content-between">
+                    <span className="label">Amount</span>
+                    <span className="label">Balance: 0 ETH</span>
+                  </div>
+                  <div className="d-flex">
+                    <input
+                      className="custom-input me-3"
+                      type="number"
+                      required
+                      type="number"
+                      name="amount"
+                      placeholder="Bid Price"
+                      onChange={(e) => calculateAmountFromPrice(e.target.value)}
+                    />
+                    <input
+                      className="custom-input ms-3"
+                      disabled
+                      type="number"
+                      required
+                      type="number"
+                      name="amount"
+                      placeholder="Bid Amount"
+                      value={amount}
+                    />
+                  </div>
+                  <Button
+                    onClick={handleClick}
+                    disabled={amount > 0 && isWeb3Connected ? false : true}
+                    className="sub-btn mt-3"
+                  >
+                    GO
+                  </Button>
+                  <p
+                    style={{
+                      color: "red",
+                      marginTop: "5px",
+                      textAlign: "center",
+                      fontSize: "14px",
+                    }}
+                  >
+                    warning: SeedHub does not support deflationary tokens
+                  </p>
+                </Col>
+              ) : (
+                <Col
+                  md={6}
+                  lg={5}
+                  className="offset-lg-2 mt-4 mt-md-0 p-4 p-md-5 bg-off"
                 >
-                  warning: SeedHub does not support deflationary tokens
-                </p>
-              </Col>
+                  {true ? (
+                    <>
+                      <div className="form-heading text-center mb-4">
+                        Claim For Pool
+                      </div>
+                      <div className="text-center fs-6">
+                        <Countdown
+                          key={1}
+                          date={
+                            new Date(location.state.claimAuctionFundsAt * 1000)
+                          }
+                          renderer={renderer}
+                          onComplete={() => {
+                            setIsExpired(true);
+                          }}
+                        />
+                        <Button
+                          disabled={isWeb3Connected ? false : true}
+                          className="sub-btn mt-3"
+                        >
+                          Claim Funds
+                        </Button>
+                      </div>
+                    </>
+                  ) : (
+                    <div className="form-heading text-center mb-4">Expired</div>
+                  )}
+                </Col>
+              )}
             </Row>
           </form>
         </div>
